9f1f9f807b0e6a449677ecfe179841b67b5593f4
KomalBarnwal <53593781+KomalBarnwal@users.noreply.github.com>
2022-03-14 20:56:12 +0530

    Update assignment1Q5.java

--- Core Java 11/Basic DS Assignments/assignment1Q5.java
+++ Core Java 11/Basic DS Assignments/assignment1Q5.java
@@ -4,19 +4,22 @@ class TaxAmount{
     public double calculateTaxAmount(int ctc){
     	//SlabA
     	if(ctc>0 && ctc<=180000) {
-    		System.out.println("Tax amount is:"+tax);
+    		System.out.println("This Annual amount is for SlabA: "+ctc+" and the tax for SlabA is :"+tax);
     	}
     	//SlabB
     	else if(ctc>181001 && ctc<=300000) {
-    		System.out.println("Tax amount is:"+tax);
+    		tax=0.1*ctc;
+    		System.out.println("This Annual amount is for SlabB: "+ctc+" and the tax for SlabB is :"+tax);
     	}
     	//SlabC
     	else if(ctc>300001 && ctc<=500000) {
-    		System.out.println("Tax amount is:"+tax);
+    		tax=0.2*ctc;
+    		System.out.println("This Annual amount is for SlabC: "+ctc+" and the tax for SlabC is :"+tax);
     	}
     	//SlabD
     	else if(ctc>500001 && ctc<=1000000) {
-    		System.out.println("Tax amount is:"+tax);
+    		tax=0.3*ctc;
+    		System.out.println("This Annual amount is for SlabD: "+ctc+" and the tax for SlabD is :"+tax);
     	}
     	return tax;
     }
